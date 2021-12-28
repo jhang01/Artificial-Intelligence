12ff41731d6fa405575b4627110dcfb0e0232e52
tiger3132 <32318048+tiger3132@users.noreply.github.com>
2021-12-28 20:01:49 +0700

    UI design contribution

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+import nlp
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    
+    return userText
+    
 
 if __name__ == '__main__':
     app.run()
