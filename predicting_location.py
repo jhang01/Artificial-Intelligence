6e5b421b6dcaa45158e6bdfee3e57f4f7182b3dd
tiger3132 <32318048+tiger3132@users.noreply.github.com>
2021-12-30 23:28:40 +0700

    Unit testing predict location

--- predicting_location.py
+++ predicting_location.py
@@ -1,5 +1,6 @@
 from pandas import *
 import difflib
+import unittest
 
 guessed_station = False
 
@@ -8,15 +9,35 @@ data = read_csv("stations.csv")
 cities = data['name'].tolist()
 
 def predict_location(location):
-    close_matches = difflib.get_close_matches(location, cities, 1)
-    if location in cities:
-        return location
+    uppercase_location = location.upper()
+    close_matches = difflib.get_close_matches(uppercase_location, cities, 1, 0.7)
+    if uppercase_location in cities:
+        return uppercase_location
     elif len(close_matches) > 0:
         guessed_station = True
         return close_matches[0]
     else:
         return None
+
+class PredictLocationTest(unittest.TestCase):
+
+    def test_same_name(self):
+        self.assertEqual(predict_location('norwich'), 'NORWICH')
+
+    def test_similar_name(self):
+        self.assertEqual(predict_location('marke'), 'MARSKE')
         
+    def test_not_found(self):
+        self.assertEqual(predict_location('blaaa'), None)
+       
 
 if __name__ == '__main__':
-    print(predict_location("liverpool street".upper()))
+    # unittest.main()
+    print('Input value: liverpool street')
+    print('Output value: ' + predict_location("liverpool street") + '\n')
+    print('Input value: norwich')
+    print('Output value: ' + predict_location('norwich') + '\n')
+    print('Input value: marke')
+    print('Output value: ' + predict_location('marke') + '\n')
+    print('Input value: blaaa')
+    print('Output value: ' + str(predict_location('blaaa')))
