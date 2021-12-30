3014f6a296302ad5791f6d76a206e37f647ebc16
shiiyinggg <80258031+shiiyinggg@users.noreply.github.com>
2021-12-30 18:26:57 +0000

    database connection

---
+++ database_connection.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+# Connect to the database
+# The database create table is in a text file called database, just copy and run it in pgadmin
+# Need to create a database in pgadmin first and change these parameters into your own details to allow succesful connection
+conn = psycopg2.connect(database = 'ai', user = 'postgres', password='password',host='127.0.0.1', port='5432')
+# Create a cursor
+cursor = conn.cursor()
+# Execute the command
+cursor.execute('SELECT * FROM traindata')
+# Get all the row in traindata
+data = cursor.fetchall()
+print(data)
+# Close connection
+conn.close()
