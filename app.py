7e3510b29c4acec1cb301f72e25414665004401c
Jason Hang <jasonhang@outlook.com>
2022-01-05 19:19:05 +0000

    Early KB
    Just greeting

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from flask import render_template
 import nlp
+import kb
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -10,9 +12,17 @@ def index():
 
 @app.route("/get")
 def get_bot_response():
+    what_to_respond()
+    response = kb.response
+    return response
+
+def what_to_respond():
     userText = request.args.get('msg')
-    return userText
-    
+    if userText in nlp.greeting_input:
+        kb.process_entities('greeting')
+    else:
+        kb.response = ("Don't understand")
+
 
 if __name__ == '__main__':
     app.run()
