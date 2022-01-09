e5fb13561907330b01516e574f4a8498e54b8ae6
shiiyinggg <80258031+shiiyinggg@users.noreply.github.com>
2022-01-09 16:14:12 +0000

    insert conversation details into database

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request
 from flask import render_template
 import nlp
 import kb
+import psycopg2
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -14,6 +14,23 @@ def index():
 def get_bot_response():
     what_to_respond()
     response = kb.response
+    try:
+        userText = request.args.get('msg')
+        conn = psycopg2.connect(database='ai', user='postgres', password='password', host='127.0.0.1', port='5432')
+        cursor = conn.cursor()
+        query = """INSERT INTO conversationdetails (user_input, response) VALUES (%s, %s) """
+        data = (userText, response)
+        cursor.execute(query, data)
+        conn.commit()
+        print("Insert data successfully")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed executing query", error)
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+            print("Database connection close")
     return response
 
 def what_to_respond():
@@ -22,7 +39,6 @@ def what_to_respond():
         kb.process_entities('greeting')
     else:
         kb.response = ("Don't understand")
-
 
 if __name__ == '__main__':
     app.run()
